30e8f34eeb755bd5c57d1f397bc200acf9576b1b
Marius Storhaug <marstor@hotmail.com>
2024-02-24 14:37:15 +0100

    test

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -33,7 +33,7 @@ $modules.Name | Select-Object -Unique | ForEach-Object {
 }
 
 Start-LogGroup 'Environment Variables'
-Write-Verbose (Get-ChildItem -Path Env: | Select-Object -Property Name, Value | Sort-Object -Property Name | Out-String)
+Write-Verbose (Get-ChildItem -Path env: | Select-Object -Property Name, Value | Sort-Object -Property Name| Format-Table | Out-String)
 Stop-LogGroup
 
 Start-LogGroup 'Files and Folders'
