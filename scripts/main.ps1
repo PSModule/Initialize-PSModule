201d52fa4d64a1a5fe79c22385d1672236fcd34d
Marius Storhaug <marstor@hotmail.com>
2024-03-03 10:29:39 +0100

    Initial version

---
+++ scripts/main.ps1
@@ -0,0 +1,42 @@
+﻿#REQUIRES -Modules Utilities
+
+[CmdletBinding()]
+param()
+
+Install-PSResource -Name 'powershell-yaml', 'Pester', 'PSScriptAnalyzer', 'platyPS' -TrustRepository -Verbose
+Stop-LogGroup
+
+Start-LogGroup 'Loading helper scripts'
+Get-ChildItem -Path (Join-Path -Path $env:GITHUB_ACTION_PATH -ChildPath 'scripts' 'helpers') -Filter '*.ps1' -Recurse |
+    ForEach-Object { Write-Verbose "[$($_.FullName)]"; . $_.FullName }
+Stop-LogGroup
+
+Start-LogGroup 'Loading inputs'
+$env:GITHUB_REPOSITORY_NAME = $env:GITHUB_REPOSITORY -replace '.+/'
+Set-GitHubEnv -Name 'GITHUB_REPOSITORY_NAME' -Value $env:GITHUB_REPOSITORY_NAME
+Stop-LogGroup
+
+Start-LogGroup 'PSVersionTable'
+Write-Verbose ($PSVersionTable | Format-Table -AutoSize | Out-String)
+Stop-LogGroup
+
+Start-LogGroup 'Installed Modules - List'
+$modules = Get-PSResource | Sort-Object -Property Name
+Write-Verbose ($modules | Select-Object Name, Version, CompanyName, Author | Out-String)
+Stop-LogGroup
+
+$modules.Name | Select-Object -Unique | ForEach-Object {
+    $name = $_
+    Start-LogGroup "Installed Modules - Details - [$name]"
+    Write-Verbose ($modules | Where-Object Name -EQ $name | Select-Object * | Out-String)
+    Stop-LogGroup
+}
+
+Start-LogGroup 'Environment Variables'
+Write-Verbose (Get-ChildItem -Path env: | Select-Object -Property Name, Value |
+        Sort-Object -Property Name | Format-Table -AutoSize -Wrap | Out-String)
+Stop-LogGroup
+
+Start-LogGroup 'Files and Folders'
+Write-Verbose (Get-ChildItem -Path $env:GITHUB_WORKSPACE -Recurse | Select-Object -ExpandProperty FullName | Sort-Object | Out-String)
+Stop-LogGroup
