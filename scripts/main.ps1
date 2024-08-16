69c90447f6f2d4d2d71c2bf3d4c29cb0a78ec75d
Marius Storhaug <marstor@hotmail.com>
2024-08-16 19:51:37 +0200

    test

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -3,13 +3,16 @@
 [CmdletBinding()]
 param()
 
-'Utilities', 'powershell-yaml', 'PSSemVer', 'Pester', 'PSScriptAnalyzer', 'platyPS' | ForEach-Object {
+$requiredModules = 'Utilities', 'powershell-yaml', 'PSSemVer', 'Pester', 'PSScriptAnalyzer', 'platyPS'
+
+$requiredModules | ForEach-Object {
     $moduleName = $_
     LogGroup "Installing prerequisite: [$moduleName]" {
         Install-PSResource -Name $moduleName -TrustRepository -Repository PSGallery
-        Write-Verbose (Get-PSResource -Name $moduleName | Select-Object *)
     }
 }
+
+$requiredModules | Get-PSResource -Name $moduleName
 
 # LogGroup 'Loading helper scripts' {
 #     Get-ChildItem -Path (Join-Path -Path $env:GITHUB_ACTION_PATH -ChildPath 'scripts\helpers') -Filter '*.ps1' -Recurse |
