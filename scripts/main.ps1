d8baf5e28bd5f9a87a344cb6cfefda59fb8b8773
Marius Storhaug <marstor@hotmail.com>
2024-08-16 20:19:57 +0200

    test

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -9,11 +9,12 @@ $requiredModules | ForEach-Object {
     $moduleName = $_
     LogGroup "Installing prerequisite: [$moduleName]" {
         Install-PSResource -Name $moduleName -TrustRepository -Repository PSGallery
-        Write-Verbose (Get-PSResource -Name $moduleName -Verbose:$false) -Verbose
+        Write-Verbose (Get-PSResource -Name $moduleName) -Verbose
     }
 }
 
-Get-PSResource -Name $requiredModules -Verbose:$false | Sort-Object -Property Name | Format-Table -AutoSize -Wrap
+Get-PSResource -Name $requiredModules -Verbose:$false | Sort-Object -Property Name |
+    Format-Table -Property Name, Version, Prerelease, Repository -AutoSize -Wrap
 
 # LogGroup 'Loading helper scripts' {
 #     Get-ChildItem -Path (Join-Path -Path $env:GITHUB_ACTION_PATH -ChildPath 'scripts\helpers') -Filter '*.ps1' -Recurse |
