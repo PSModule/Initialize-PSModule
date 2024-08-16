23e1d71008de478b6fa3f6564d3297fc3ae87aaf
Marius Storhaug <marstor@hotmail.com>
2024-08-16 20:31:55 +0200

    Cleanup

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -15,8 +15,3 @@ $requiredModules | Sort-Object | ForEach-Object {
 
 Get-PSResource -Name $requiredModules -Verbose:$false | Sort-Object -Property Name |
     Format-Table -Property Name, Version, Prerelease, Repository -AutoSize -Wrap
-
-# LogGroup 'Loading helper scripts' {
-#     Get-ChildItem -Path (Join-Path -Path $env:GITHUB_ACTION_PATH -ChildPath 'scripts\helpers') -Filter '*.ps1' -Recurse |
-#         ForEach-Object { Write-Verbose "[$($_.FullName)]"; . $_.FullName }
-# }
