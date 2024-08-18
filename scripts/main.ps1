1262ae1004bc3f1d8de2fc4e7b4471688280e6a6
Marius Storhaug <marstor@hotmail.com>
2024-08-18 10:14:51 +0200

    🩹 [Patch]: Move to `GitHub-Script` action (#19)
    ## Description
    
    - Move to `GitHub-Script` action
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,47 +1,17 @@
-﻿#REQUIRES -Modules Utilities
+﻿#Requires -Modules GitHub
 
 [CmdletBinding()]
 param()
 
-Install-PSResource -Name 'powershell-yaml', 'PSSemVer', 'Pester', 'PSScriptAnalyzer', 'platyPS' -TrustRepository -Verbose
-Stop-LogGroup
+$requiredModules = 'Utilities', 'powershell-yaml', 'PSSemVer', 'Pester', 'PSScriptAnalyzer', 'platyPS'
 
-Start-LogGroup 'Loading helper scripts'
-Get-ChildItem -Path (Join-Path -Path $env:GITHUB_ACTION_PATH -ChildPath 'scripts\helpers') -Filter '*.ps1' -Recurse |
-    ForEach-Object { Write-Verbose "[$($_.FullName)]"; . $_.FullName }
-Stop-LogGroup
-
-Start-LogGroup 'Loading inputs'
-$env:GITHUB_REPOSITORY_NAME = $env:GITHUB_REPOSITORY -replace '.+/'
-Set-GitHubEnv -Name 'GITHUB_REPOSITORY_NAME' -Value $env:GITHUB_REPOSITORY_NAME
-Stop-LogGroup
-
-Start-LogGroup 'PSVersionTable'
-Write-Verbose ($PSVersionTable | Format-Table -AutoSize | Out-String)
-Stop-LogGroup
-
-Start-LogGroup 'Installed Modules - List'
-$modules = Get-PSResource | Sort-Object -Property Name
-Write-Verbose ($modules | Select-Object Name, Version, CompanyName, Author | Out-String)
-Stop-LogGroup
-
-$modules.Name | Select-Object -Unique | ForEach-Object {
-    $name = $_
-    Start-LogGroup "Installed Modules - Details - [$name]"
-    Write-Verbose ($modules | Where-Object Name -EQ $name | Select-Object * | Out-String)
-    Stop-LogGroup
+$requiredModules | Sort-Object | ForEach-Object {
+    $moduleName = $_
+    LogGroup "Installing prerequisite: [$moduleName]" {
+        Install-PSResource -Name $moduleName -TrustRepository -Repository PSGallery
+        Write-Verbose (Get-PSResource -Name $moduleName | Select-Object * | Out-String)
+    }
 }
 
-Start-LogGroup 'Environment Variables'
-Write-Verbose (Get-ChildItem -Path env: | Select-Object -Property Name, Value |
-        Sort-Object -Property Name | Format-Table -AutoSize -Wrap | Out-String)
-Stop-LogGroup
-
-Start-LogGroup 'PowerShell Variables'
-Write-Verbose (Get-Variable | Select-Object -Property Name, Value |
-        Sort-Object -Property Name | Format-Table -AutoSize -Wrap | Out-String)
-Stop-LogGroup
-
-Start-LogGroup 'Files and Folders'
-Write-Verbose (Get-ChildItem -Path $env:GITHUB_WORKSPACE -Recurse | Select-Object -ExpandProperty FullName | Sort-Object | Out-String)
-Stop-LogGroup
+Get-PSResource -Name $requiredModules -Verbose:$false | Sort-Object -Property Name |
+    Format-Table -Property Name, Version, Prerelease, Repository -AutoSize -Wrap
