c927d0cdb1b86a2e5b453dec04ccbcd60238b960
Marius Storhaug <marstor@hotmail.com>
2024-11-27 18:28:23 +0100

    🩹 [Patch]: Separate preview modules for installation in main script

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -3,12 +3,20 @@
 [CmdletBinding()]
 param()
 
-$requiredModules = 'Utilities', 'powershell-yaml', 'PSSemVer', 'Pester', 'PSScriptAnalyzer', 'Microsoft.PowerShell.PlatyPS'
+$requiredModules = 'Utilities', 'powershell-yaml', 'PSSemVer', 'Pester', 'PSScriptAnalyzer'
+$requiredPreviewModules = 'Microsoft.PowerShell.PlatyPS'
 
 $requiredModules | Sort-Object | ForEach-Object {
     $moduleName = $_
     LogGroup "Installing prerequisite: [$moduleName]" {
         Install-PSResource -Name $moduleName -TrustRepository -Repository PSGallery
+        Write-Verbose (Get-PSResource -Name $moduleName | Select-Object * | Out-String)
+    }
+}
+$requiredPreviewModules | Sort-Object | ForEach-Object {
+    $moduleName = $_
+    LogGroup "Installing prerequisite: [$moduleName] (prerelease)" {
+        Install-PSResource -Name $moduleName -TrustRepository -Repository PSGallery -Prerelease
         Write-Verbose (Get-PSResource -Name $moduleName | Select-Object * | Out-String)
     }
 }
