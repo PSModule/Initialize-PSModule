a6f47cdbe6d5280e15b648ef40a321b5e0e6e730
Marius Storhaug <marstor@hotmail.com>
2024-11-27 19:05:40 +0100

    🩹 [Patch]: Use `Microsoft.PowerShell.PlatyPS` instead of `platyPS` (#22)
    ## Description
    
    - Use `Microsoft.PowerShell.PlatyPS` instead of `platyPS`
    - Needed by https://github.com/PSModule/Build-PSModule/issues/63
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -2,16 +2,27 @@
 
 [CmdletBinding()]
 param()
-
-$requiredModules = 'Utilities', 'powershell-yaml', 'PSSemVer', 'Pester', 'PSScriptAnalyzer', 'platyPS'
-
-$requiredModules | Sort-Object | ForEach-Object {
-    $moduleName = $_
-    LogGroup "Installing prerequisite: [$moduleName]" {
-        Install-PSResource -Name $moduleName -TrustRepository -Repository PSGallery
-        Write-Verbose (Get-PSResource -Name $moduleName | Select-Object * | Out-String)
+$requiredModules = @{
+    Utilities                      = @{}
+    'powershell-yaml'              = @{}
+    PSSemVer                       = @{}
+    Pester                         = @{}
+    PSScriptAnalyzer               = @{}
+    # PlatyPS                        = @{}
+    'Microsoft.PowerShell.PlatyPS' = @{
+        Prerelease = $true
     }
 }
 
-Get-PSResource -Name $requiredModules -Verbose:$false | Sort-Object -Property Name |
+$requiredModules.GetEnumerator() | Sort-Object | ForEach-Object {
+    $name = $_.Key
+    $settings = $_.Value
+    LogGroup "Installing prerequisite: [$name]" {
+        Install-PSResource -Name $name -TrustRepository -Repository PSGallery @settings
+        Write-Verbose (Get-PSResource -Name $name | Select-Object * | Out-String)
+        Write-Verbose (Get-Command -Module $name | Out-String)
+    }
+}
+
+$requiredModules.Keys | Get-InstalledPSResource -Verbose:$false | Sort-Object -Property Name |
     Format-Table -Property Name, Version, Prerelease, Repository -AutoSize -Wrap
